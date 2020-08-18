d798399238b80cd1636eb1f3df1fbe128258c956
Priyadebnath <69429279+Priyadebnath@users.noreply.github.com>
2020-08-18 16:29:53 +0530

    README.C
    TO CHECK IF THE ith BIT IS SET IN THE BINARY FORM OF THE GIVEN NUMBER.

---
+++ README.C
@@ -0,0 +1,14 @@
+# TTASK3
+ 
+#include<stdio.h>
+int main()
+{
+int n,i;
+bool a = n&(1<<i);
+printf("ENTER THE VALUE OF n AND i");
+scanf("%d,%d",&n,&i);
+if( a==true)
+ printf("yes, i bit is set");
+else
+ printf("no, i bit is not a set");
+}
